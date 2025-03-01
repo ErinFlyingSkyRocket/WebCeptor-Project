e5666ec0953f1bf2d239fc88fd53e2fd2edeba28
ErinFlyingSkyRocket <144037916+ErinFlyingSkyRocket@users.noreply.github.com>
2025-03-01 17:37:18 +0800

    victim

--- victim_app/mitm_startup.sh
+++ victim_app/mitm_startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-cd /home/kali/mitmproxy
-source venv/bin/activate  # Activate virtual environment (if used)
-nohup mitmdump --mode regular@8080 --listen-host 127.0.0.1 --ssl-insecure -s /home/user/mitmproxy/mitm_interceptor.py > /dev/null 2>&1 &
-echo $! > /home/kali/.mitmproxy_pid  # Store the process ID (PID) for stopping later
+
+# Navigate to the project directory
+cd ~/WebCeptor-Project
+
+# Activate the virtual environment (if exists)
+source venv/bin/activate
+
+# Run mitmdump silently with necessary flags
+nohup mitmdump --mode regular@8080 \
+    --listen-host 127.0.0.1 \
+    --ssl-insecure \
+    -s ~/WebCeptor-Project/victim_app/mitm_interceptor.py > /dev/null 2>&1 &
+
+# Store process ID for later stopping
+echo $! > ~/.mitmproxy_pid
